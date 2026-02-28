fb61aaad3725dd5ac265e16217b65646f58842fd
Abhishek Choudhery <abhishekchoudery45@gmail.com>
2026-02-28 17:12:27 +0530

    Finished Experiment 02

---
+++ 02-welcome-gulzar-group/main.py
@@ -0,0 +1,9 @@
+"""
+Experiment: 02
+Program: Basic Print and Indentation
+Author: Abhishek Choudhery
+Description: Python program to print "Welcome to Gulzar Group of Institutions" using comments and proper indentation.
+"""
+
+# Printing the welcome message
+print("Welcome to Gulzar Group of Institutions")
