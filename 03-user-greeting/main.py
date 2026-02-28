21f60072b61d670489699ec795fadf82d68ee770
Abhishek Choudhery <abhishekchoudery45@gmail.com>
2026-02-28 17:13:41 +0530

    Finished Experiment 03

---
+++ 03-user-greeting/main.py
@@ -0,0 +1,12 @@
+"""
+Experiment: 03
+Program: User Input Welcome
+Author: Abhishek Choudhery
+Description: Python program to take user name and print a welcome message.
+"""
+
+# Taking input from the user
+user_name = input("Enter your name: ")
+
+# Printing the welcome message
+print(f"Hello {user_name}, welcome to the Python Programming Lab!")
